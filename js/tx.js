f6e64a8ab0dfc489ee2f5d25315569c67ef0cd87
lpxyr <wjzcd1988@163.com>
2024-01-14 12:03:53 +0800

    Site updated: 2024-01-14 12:03:52

---
+++ js/tx.js
@@ -0,0 +1,10 @@
+if (GLOBAL_CONFIG.Snackbar) {
+  btf.snackbarShow('欢迎光临')
+}
+// window.onload = function(){
+//   document.getElementById('btn1').addEventListener('click',goToBaiDu)
+//   }
+//   function goToBaiDu(){
+//   window.open('https://pan.baidu.com/s/13D5DSlYodcKJIQOPmVmWZg')
+//   }
+ 
